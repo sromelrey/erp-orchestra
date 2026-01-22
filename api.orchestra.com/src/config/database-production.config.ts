e137342c59642339ccc7fd9b76febae239b1d43f
Romel Rey Silva <sromelrey@gmail.com>
2026-01-22 22:33:47 +0800

    remove hard coded db url

--- api.orchestra.com/src/config/database-production.config.ts
+++ api.orchestra.com/src/config/database-production.config.ts
@@ -2,7 +2,7 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  url: 'postgresql://neondb_owner:npg_DNyXM0H4ZAmz@ep-polished-math-ahy4du7r-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+  url: `${process.env.DATABASE_URL}`,
   entities: [__dirname + '/../entities/**/*.entity.{ts,js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   synchronize: false, // Keep sync disabled for production
